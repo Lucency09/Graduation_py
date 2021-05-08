c7fd7b09ab56f0c4e7c83c26012fbbfbc0d83e6e
gzgypzy@foxmail.com <43517151+Lucency09@users.noreply.github.com>
2021-05-08 14:51:45 +0800

    test

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-import numpy as np
 import cv2
 import openpose as op
+import numpy as np
 
-
-img = cv2.imread('C:/Users/Lucency/Desktop/posetest.jpg')#读图
-cv2.imshow('img',img)
- 
-keypage,keyary = op.getkey(img)
-cv2.imshow('keypoint',keypage)
-im = op.getkeyary(keyary[0])
-cv2.imshow('test',im)
-cv2.waitKey(0)
+op.Video_processing('models/Turn_Right.mp4','models/Turn_Right.npy')
